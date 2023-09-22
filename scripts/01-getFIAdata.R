a3797bcaa7dacd83766b05664803b2406faf6784
Ceci Martinez <cmartinez3@arizona.edu>
2023-09-21 20:12:49 -0700

    clean up code

--- scripts/01-getFIAdata.R
+++ scripts/01-getFIAdata.R
@@ -1,13 +1,13 @@
 ## This script gets tables from FIADB and turns them into csvs
 ## Cecilia Martinez modified by Ellen Bledsoe
-## 2023-03-15
+## March 15 2023
 ## cecimartinez333@gmail.com
 
 
 # Load Packages -----------------------------------------------------------
 
 library(DBI)#required to access FIADB
-library(tidyverse) 
+library(tidyverse)#data processing
 
 # Getting data from SQL ---------------------------------------------------
 
@@ -15,8 +15,7 @@ library(tidyverse)
 # interest to our working directory by going to FIA datamart
 # https://experience.arcgis.com/experience/3641cea45d614ab88791aef54f3a1849/
 
-# get data for Idaho
-
+# GET IDAHO DATA ----------------------------------------------------------
 # Set path to where you want the CSVs to appear
 path_out_ID <- "data_raw/"
 
@@ -25,7 +24,7 @@ con_ID <-  dbConnect(RSQLite::SQLite(), "SQLite_FIADB_ID.db")
 dbGetQuery(conn = con_ID, "SELECT name FROM sqlite_master WHERE type='table';")
 
 # subset tables that you want
-db_table_names <-
+db_file_names_ID <-
   c("PLOT",
     "TREE",
     "COND",
@@ -34,11 +33,59 @@ db_table_names <-
     "PLOT_REGEN",
     "PLOTGEOM")
 
-#loop through and write them at csvs for each specified table
-for (i in db_table_names) {
-  write.csv(dbReadTable(conn = con, name = i),
-            file = paste0(path_out, "ID_", i, ".csv"))
+#loop through specified tables and write them at csvs
+for (i in db_file_names_ID) {
+  write_csv(dbReadTable(conn = con_ID, name = i),
+            file = paste0(path_out_ID, "ID_", i, ".csv"))
+}
+
+# GET MONTANA DATA --------------------------------------------------------
+# Set path to where you want the CSVs to appear
+path_out_MT <- "data_raw/"
+
+# reference the database and checking the connection and table names
+con_MT <-  dbConnect(RSQLite::SQLite(), "SQLite_FIADB_MT.db")
+dbGetQuery(conn = con_MT, "SELECT name FROM sqlite_master WHERE type='table';")
+
+# subset tables that you want
+db_file_names_MT <-
+  c("PLOT",
+    "TREE",
+    "COND",
+    "SUBPLOT_REGEN",
+    "SUBPLOT",
+    "PLOT_REGEN",
+    "PLOTGEOM")
+
+#loop through specified tables and write them at csvs
+for (i in db_file_names_MT) {
+  write_csv(dbReadTable(conn = con_MT, name = i),
+            file = paste0(path_out_MT, "MT_", i, ".csv"))
+}
+
+# GET WYOMING DATA --------------------------------------------------------
+# Set path to where you want the CSVs to appear
+path_out_WY <- "data_raw/"
+
+# reference the database and checking the connection and table names
+con_WY <-  dbConnect(RSQLite::SQLite(), "SQLite_FIADB_WY.db")
+dbGetQuery(conn = con_WY, "SELECT name FROM sqlite_master WHERE type='table';")
+
+# subset tables that you want
+db_file_names_WY <-
+  c("PLOT",
+    "TREE",
+    "COND",
+    "SUBPLOT_REGEN",
+    "SUBPLOT",
+    "PLOT_REGEN",
+    "PLOTGEOM")
+
+#loop through specified tables and write them at csvs
+for (i in db_file_names_WY) {
+  write_csv(dbReadTable(conn = con_WY, name = i),
+            file = paste0(path_out_WY, "WY_", i, ".csv"))
 }
 
 #close connection
-dbDisconnect(con)
+dbDisconnect(con_ID, con_MT, con_WY)
