17763ef4a25fb4941df3b895716f899204135153
Ceci Martinez <cmartinez3@arizona.edu>
2023-09-25 16:50:01 -0700

    more growth modeling

--- scripts/growth-model/growth-model.R
+++ scripts/growth-model/growth-model.R
@@ -11,6 +11,7 @@ library(lme4)
 library(lmerTest)
 library(MuMIn) # use MuMin to choose between models (AICc)
 library(DHARMa)
+library(performance)
 
 
 # Read in data used for model ---------------------------------------------
@@ -18,43 +19,42 @@ library(DHARMa)
 model_growth_data <- read_csv("data_processed/WBP_growth.csv")
 
 model_growth_data <- model_growth_data %>% dplyr::select(TRE_CN, PREV_TRE_CN, PLT_CN, PREV_PLT_CN, LAT, LON, ELEV, 
-                                                  DIA, PREVDIA, DIA_DIFF, MEASYEAR, PREV_MEASYEAR, CENSUS_INTERVAL, 
-                                                  BALIVE, CONDID, STATUSCD, DSTRBCD1, DSTRBCD2, DISTRBCD3, AGENTCD)
+                                                  DIA, PREVDIA, DIA_DIFF, DIA_INCR, MEASYEAR, PREV_MEASYEAR, CENSUS_INTERVAL, 
+                                                  BALIVE, CONDID, STATUSCD, DSTRBCD1, DSTRBCD2, DSTRBCD3, AGENTCD)
 
-#scale numerical covariates (just BALIVE at this point, lat, lon, elev)
+#standardize numerical covariates, center and scale 
 
-scaled_grow_data <- grow_data_model %>% mutate_at(scale, .vars = vars(-TRE_CN, -PREV_TRE_CN, -PLT_CN, -PREV_PLT_CN, -CONDID,
+scaled_growth_data <- model_growth_data %>% mutate_at(scale, .vars = vars(-TRE_CN, -PREV_TRE_CN, -PLT_CN, -PREV_PLT_CN, -CONDID,
                                                                       -MEASYEAR, -PREV_MEASYEAR,
-                                                                      -CENSUS_INTERVAL, -DIA_INCR, -STATUSCD ))
+                                                                      -CENSUS_INTERVAL, -DIA_INCR, -STATUSCD))
 
 
+#growth model that includes plot level random effect
 
-#growth model 
+#this is the linear model 
+grow_model_lin <- lmer(DIA_INCR ~ PREVDIA + BALIVE + (1|PLT_CN), data = scaled_growth_data)
+class(grow_model_lin) <- "lmerMOD"
 
-hist(grow_data_model$DIA_INCR) #looks normally distributed - some negative values, itneresting, how to correct for this
+# check the model residuals in DHARMA: not looking so great 
+plot(simulateResiduals(grow_model_lin, integerResponse = F), quantreg = T)
+check_model(grow_model_lin)
+#how to graph residuals with mixed models?? - ask for help
 
-#growth mode, and plot level random effect
-gmodel.DIA_lin <- lmer(DIA_INCR ~ PREVDIA + BALIVE + (1|PLT_CN), data = scaled_grow_data)
-plot(simulateResiduals(gmodel.DIA_lin, integerResponse = F), quantreg = T)
-
-#how to graph residuals with mixed models
-
-#check the residuals 
-plot(gmodel.DIA_lin)
+#check the residuals using DHARMA package
+plot(grow_model_lin)
 #quantile deviations detected 
 #model prediction - rank transformed
-#gamma distirbution? data is skewed
 
-#quadratic effect
-gmodel.DIA.q <- lmer(DIA_INCR ~ PREVDIA + BALIVE  + 
+#quadratic effects - check preference for model by using AICc
+grow_model_quad <- lmer(DIA_INCR ~ PREVDIA + BALIVE  + 
                        I(PREVDIA^2) + I(BALIVE^2) +
-                       (1|PLT_CN), data = scaled_grow_data)
-qqnorm(residuals(gmodel.DIA.q))
+                       (1|PLT_CN), data = scaled_growth_data)
+qqnorm(residuals(grow_model_quad))
 
-check_model(gmodel.DIA.q)
-summary(gmodel.DIA.q)
-plot(simulateResiduals(gmodel.DIA.q, integerResponse = F), quantreg = T)
-plot(gmodel.DIA.q)
+check_model(grow_model_quad)
+summary(grow_model_quad)
+plot(simulateResiduals(grow_model_quad, integerResponse = F), quantreg = T)
+plot(grow_model_quad)
 
 resid_pearson <- residuals(gmodel.DIA.q,type="pearson",scaled=TRUE)
 predicted <- predict(gmodel.DIA.q)
